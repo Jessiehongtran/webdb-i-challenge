b183e46f969af481f84cc07b1d7f7796c0fbb2f7
Your Name <you@example.com>
2019-09-09 14:44:22 -0700

    got data for GET with id

--- server.js
+++ server.js
@@ -7,20 +7,28 @@ const server = express();
 server.use(express.json());
 
 server.get('/accounts', (req,res)=> {
-db.select('*')
-    .from('accounts')
-    .then(customers => {
+    db.select('*')
+        .from('accounts')
+        .then(customers => {
         res.status(200).json(customers)
-    })
-    .catch(err => res.json(err))
+        })
+        .catch(err => res.json(err))
 })
 
 server.get('/accounts/:id', (req,res)=> {
-    
+    const {id} = req.params;
+    db('accounts').where({id})
+        .first()
+        .then(accounts => {
+            res.status(200).json(accounts)
+        }) 
+        .catch(err => res.json(err))
 })
 
 server.post('/accounts', (req,res)=> {
-    
+    const postData = req.body
+    console.log(postData)
+    db('accounts').insert(postData,)
 })
 
 server.put('/accounts/:id', (req,res)=> {
