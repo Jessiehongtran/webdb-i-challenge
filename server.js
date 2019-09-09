81b0b9180d8f1f3e55696203fb3bf1f0355faa58
Your Name <you@example.com>
2019-09-09 14:38:53 -0700

    got data back from GET request

--- server.js
+++ server.js
@@ -6,4 +6,28 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/accounts', (req,res)=> {
+db.select('*')
+    .from('accounts')
+    .then(customers => {
+        res.status(200).json(customers)
+    })
+    .catch(err => res.json(err))
+})
+
+server.get('/accounts/:id', (req,res)=> {
+    
+})
+
+server.post('/accounts', (req,res)=> {
+    
+})
+
+server.put('/accounts/:id', (req,res)=> {
+    
+})
+
+server.delete('/accounts/:id', (req,res)=> {
+    
+})
 module.exports = server;
